d48728fadf15b338daf6b439ff7d1175c8be5817
miguelsilva1234 <161628496+miguelsilva1234@users.noreply.github.com>
2025-03-07 16:59:24 -0300

    Add files via upload

---
+++ Aula04/Exercicio01/exe01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <form action="exe01.php" method="POST">                
+        <div class="mb-3">
+            <label class="form-label">Digite seu nome:</label>
+            <input type="number" id="quantity" name="quantity" min="0" max="10">
+            <input type="number" id="quantity" name="quantity" min="0" max="10">
+            <input type="number" id="quantity" name="quantity" min="0" max="10">
+        </div>       
+        <input class="btn btn-primary" type="submit" value="ENVIAR">                
+    </form>
+</body>
+</html>
